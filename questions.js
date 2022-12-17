2f52c77ff8f8ddbd869967f32296f9897102e15e
rforaker <rtforaker@gmail.com>
2022-12-17 10:48:13 -0500

    Created questions in js

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: " A commonly used data type in Javascript is:",
+        choices: ["string", "alert", "modal", "button"],
+        answer: "string"
+    },
+    {
+        title: "The === operator returns true if:",
+        choices: ["If the data is equal", "If the data is not equal", "If the data is equal, and the same data type", "It never returns true"],
+        answer: "If the data is equal, and the same data type"
+    },
+    {
+        title: "A variable in Javascript can store:",
+        choices: ["numbers and strings", "arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "To make a number in a variable a string instead of an integer, you would wrap it in:",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "Javascript is also known as:",
+        choices: ["Java", "ECMAscript", "CSS", "Algorithm"],
+        answer: "ECMAscript"
+    },
+
+];
